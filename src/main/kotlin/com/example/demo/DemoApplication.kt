a26832e2dcc49c627c0206eaa9a711acadee8b2c
Marco Davi <mardavi84@gmail.com>
2021-10-21 08:42:48 +0200

    sample

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,23 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+	@GetMapping
+	fun test(@RequestParam(defaultValue = "#{{}}") list: List<String>) =
+		list
+}
